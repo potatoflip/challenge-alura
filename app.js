8457f175926f53a7e04209b97c33bf20f5cb1c32
Carlos <carlitoseduardo147@gmail.com>
2025-08-05 13:30:02 -0400

    Primeros cambios realizados, intentando entender la estructura del challenge, Aun no funcion

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = [];
+let listaAmigos = 0;
+
+function agregarNombre(){
+  let listaAmigos = document.getElementById("amigo")
+  if (listaAmigos == ""){
+    alert("Por favor ingresen un valor")
+    }
+  else {
+    nombreAmigos.push(listaAmigos)
+    }
+}
+agregarNombre();
+console.log(agregarNombre());
+console.log(nombreAmigos);
