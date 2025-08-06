b3df4c588007baec0172bf8e59eb8ff9d66f73c1
Carlos <carlitoseduardo147@gmail.com>
2025-08-06 13:11:27 -0400

    mejora eficiencia en el codigo con funciones, se añade el sorteo del amigo, y se modifica la verificacion para que no se ingresen numeros

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombreAmigos = [];
-
+//Funcion permite verificar que el input no este vacio o sea un numero, al ser cualquiera de los casos muestra una alerta.
 function agregarNombre(){
   let listaAmigos = document.getElementById("amigo").value.trim();
   if (listaAmigos == ""){
-    alert("Por favor ingresen un valor");
+    alert("Por favor ingresen un nombre");
+    }
+    else if(!isNaN(listaAmigos)){
+      alert("Por favor ingrese un nombre")
     }
   else {
     nombreAmigos.push(listaAmigos);
@@ -13,16 +16,34 @@ function agregarNombre(){
     limpiarTexto();
     console.log(nombreAmigos);
 }
+
+// Toma los nombres que se ingresan y los va añadiendo al elemento lista para mostrar en pantalla
+function mostrarLista(){
+  let longitudLista = nombreAmigos.length;
+  let lista = "";
+  for (let i = 0; i < longitudLista; i++){
+    lista += `<li>${nombreAmigos[i]}</li>`;
+  }
+  mostrarTextos("listaAmigos", lista)
+  return lista;
+}
+//utiliza la funcion random para elegir un nombre al azar segun las posiciones de la lista.
+function sortearAmigo(){
+  let longitudLista = nombreAmigos.length;
+  if (nombreAmigos == ""){
+    alert("Ingrese al menos un nombre");
+  }
+  else {
+    let sorteo = nombreAmigos[Math.floor(Math.random()*(longitudLista))];
+    mostrarTextos("resultado",sorteo);
+  }
+  return sorteo;
+}
+//Funciones llamadas
 function limpiarTexto(){
   document.getElementById("amigo").value = "";
 }
 
-function mostrarLista(){
-  let longitudLista = nombreAmigos.length;
-  let lista = document.getElementById("listaAmigos");
-  lista.innerHTML = "";
-  for (let i = 0; i < longitudLista; i++){
-    lista.innerHTML += `<li>${nombreAmigos[i]}</li>`;
-  }
-  return lista.innerHTML;
+function mostrarTextos(idElemento, texto){
+  document.getElementById(idElemento).innerHTML = texto;
 }
